c34a5a965a5eabfd812e9dc00edb2e7f3b940e51
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-12-06 17:59:46 -0200

    Inicio Array Parte 02

---
+++ #Seção 05 - Arrays/#Seção 05 - Arrays.R
@@ -0,0 +1,13 @@
+#Seção 05 - Arrays
+
+######################### Array, Criação  ###############################
+
+#Seção 05 - Arrays - Parte 01
+
+M1<-matrix(1:9,3,3)
+M1
+
+A<-array(1:9,dim=c(3,3,3))
+A
+
+#Seção 05 - Arrays - Parte 02
