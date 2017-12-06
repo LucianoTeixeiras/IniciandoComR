e5b70445534cfd208cf92269b3b26d31869eab96
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-12-06 14:30:56 -0200

    Código Fonto da Seção 01

---
+++ #Seção 01 - Introdução/#Seção 01 - Introdução.R
@@ -0,0 +1,26 @@
+#Seção 01 - Introdução
+
+######################### Apresentação: A Importnacia do R  #############
+
+######################### Instalações Necessárias #######################
+
+# - Instalação do R
+
+# - Instalação do R Studio
+
+######################### Citações e Demo em R  #########################
+
+#Citação
+citation()
+demo(persp)
+demo(graphics)
+
+######################### R Como Calculadora  ###########################
+
+# Quiz - Pergunta 1
+a<- 3+(4*5)-(76/2)
+b<- 3+(4*5)-(76/2)
+c<- (3+4)*5-76/2
+a
+b
+c
