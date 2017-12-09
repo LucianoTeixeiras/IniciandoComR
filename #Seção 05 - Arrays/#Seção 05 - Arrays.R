c895222de21b6048db81e5ba197ca62f63b41a1f
LTS-02\luciano <luciano.teixeiras@gmail.com>
2017-12-09 18:10:24 -0200

    Array de Vetores

--- #Seção 05 - Arrays/#Seção 05 - Arrays.R
+++ #Seção 05 - Arrays/#Seção 05 - Arrays.R
@@ -12,4 +12,23 @@ A
 
 #Seção 05 - Arrays - Parte 02
 
-#Coninuando
+A<-array(1:16, dim = c(4,4,3))
+A
+
+A<-array(c("Esteio","Sapucaia"), dim = c(4,4,4))
+A
+
+dim(A)
+
+######################### Array: Colecao de Matrizes/Vetores
+
+vetor1<-c(20,40,50)
+
+vetor2<-c("Camila","Paulo","Pedro")
+
+vetor3<-c("Camila","Paulo","Pedro","Marcos","Maria")
+
+B<-array(c(vetor1,vetor2), dim = c(3,4,2))
+B
+
+#Seção 05 - Arrays - Parte 03
