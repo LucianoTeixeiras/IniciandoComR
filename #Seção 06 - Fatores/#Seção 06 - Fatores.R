c226f2f36766443b1d296a4ffd2f2cfafdba9b2c
LTS-02\luciano <luciano.teixeiras@gmail.com>
2017-12-14 00:02:08 -0200

    #Seção 06 - Fatores Fim

--- #Seção 06 - Fatores/#Seção 06 - Fatores.R
+++ #Seção 06 - Fatores/#Seção 06 - Fatores.R
@@ -1,1 +1,13 @@
 #Seção 06 - Fatores
+
+est_civ<-c("Casado","Solteiro","Divorciado","Casado","casado","Outro","Solteiro","Outros","Solteiro")
+est_civ
+
+A<-factor(est_civ,levels = c("Soleiro","Casado","Divorciado","Outro"))
+A
+
+estadp_civil<-gl(4,4)
+estadp_civil
+
+estadp_civil<-gl(4,2, lables = c("Solteiro","Casado","Divorciado","Outro"))
+estadp_civil
