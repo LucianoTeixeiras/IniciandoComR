db8f2153d9634c03448bed840df44eb2b17e8ed8
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-12-18 11:47:47 -0200

    ######################### Dataframes: Criação

--- #Seção 08 - Dataframes/#Seção 08 - Dataframes.R
+++ #Seção 08 - Dataframes/#Seção 08 - Dataframes.R
@@ -2,3 +2,21 @@
 
 ######################### Dataframes: Criação
 
+#Dataframes: Planilha de Dados
+
+Nome<-c("João","Maria","Marcos")
+Idade<-c(23,34,50)
+Genero<-c("Masculino","Feminino","Masculino")
+dados<-data.frame(Nome,Idade,Genero)
+dados
+str(dados)
+dim(dados)
+
+ncol(dados)
+nrow(dados)
+colnames(dados)
+names(dados)
+summary(dados)
+
+######################### Dataframes: Acessando seus Elementos
+
