9e5bf030609b9f3df9774954093467ce730dce72
Luciano Teixeira <luciano.teixeiras@gmail.com>
2017-12-18 12:20:23 -0200

    ######################### Dataframes: Fim

--- #Seção 08 - Dataframes/#Seção 08 - Dataframes.R
+++ #Seção 08 - Dataframes/#Seção 08 - Dataframes.R
@@ -20,3 +20,66 @@ summary(dados)
 
 ######################### Dataframes: Acessando seus Elementos
 
+dados[1,]
+dados[3,]
+dados[1:2,]
+dados[c(1,3),]
+dados[,2]
+dados[,3]
+dados[,2:3]
+dados[2,2]
+dados[2:3,1:2]
+names(dados)
+dados$Nome
+dados$Idade
+
+a<-dados$Nome
+a
+
+######################### Dataframes: Modificando seus Elementos
+
+dados[1,1]
+dados$Idade[3]<-90
+dados
+
+dados$Nome<-c("Luiz","Carla","Manoel")
+dados
+
+######################### Dataframes: Adicionando e Removendo Variáveis
+
+dados
+dados$Cidade<-c("Poa","Esteio","Sapuca")
+dados
+
+dados$Renda<-c(4000,5000,6000)
+dados
+
+dados<-dados[,-5]
+dados
+
+dados<-dados[,-4]
+dados
+
+######################### Dataframes: Adicionando e Removendo Casos no Dataframe
+
+#Caso desse erro, tranform Nomes em Caracter: dados$Nomes<-as.character(Dados$Nomes)
+
+dados
+dados[4,]<-c("Paula","70","Feminino")
+dados
+str(dados)
+
+dados[4,]<-c("Lucas","20","Masculino")
+dados
+
+str(dados)
+
+dados$Idade<-as.numeric(dados$Idade)
+str(dados)
+dados
+
+dados[-4,]
+dados<-dados[-4,]
+dados
+
+######################### Dataframes: Fim
